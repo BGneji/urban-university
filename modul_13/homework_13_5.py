95c02100339f2b3485712247f095472ad79345a0
NEJI <mrkochetov@yandex.ru>
2024-08-26 17:23:36 +0300

    homework_13_5

---
+++ modul_13/homework_13_5.py
@@ -0,0 +1,103 @@
+import logging
+
+from dotenv import load_dotenv
+import os
+
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+load_dotenv()
+
+api = os.getenv('API1')
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+# инициализация клавиатуры resize_keyboard=True подстраивает клавиатуру под интерфейс
+kb = ReplyKeyboardMarkup(resize_keyboard=True)
+button = KeyboardButton(text='Информация')
+button2 = KeyboardButton(text='Рассчитать')
+# добавляет кнопку по строчно
+kb.add(button)
+kb.add(button2)
+
+
+class UserState(StatesGroup):
+    """State: age, growth, weight (возраст, рост, вес)."""
+    age = State()
+    growth = State()
+    weight = State()
+    gender = State()
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    text = 'Привет!'
+    await message.answer(text, reply_markup=kb)
+
+
+@dp.message_handler(text='Рассчитать')
+async def set_age(message):
+    await message.answer('Введите свой возраст:')
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await message.answer('Введите свой рост:')
+    await state.update_data(age=message.text)
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await message.answer('Введите свой вес:')
+    await state.update_data(growth=message.text)
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def set_weight(message, state):
+    await message.answer('У кажите совой пол:')
+    await state.update_data(weight=message.text)
+    await UserState.gender.set()
+
+
+@dp.message_handler(state=UserState.gender)
+async def send_calories(message, state):
+    await state.update_data(gender=message.text)
+    data = await state.get_data()
+    print(data)
+    if data['gender'].lower() == 'м':
+        age = 5 * int(data['age']) + 5
+        growth = 6.25 * int(data['growth'])
+        weight = 10 * int(data['weight'])
+    elif data['gender'].lower() == 'ж':
+        age = 5 * int(data['age']) - 161
+        growth = 6.25 * int(data['growth'])
+        weight = 10 * int(data['weight'])
+    res = weight + growth - age
+    print(res)
+    await message.answer(f"Ваша норма калорий {res}")
+    await state.finish()
+
+
+@dp.message_handler(text=['Информация'])
+async def info(message):
+    text = 'Привет! Я бот помогающий твоему здоровью.'
+    await message.answer(text)
+
+
+@dp.message_handler()
+async def all_massages(message):
+    """Функция для обработки всех входящих сообщений"""
+    text = 'Введите команду /start что бы начать общение'
+    await message.answer(text)
+    print('Введите команду /start, чтобы начать общение.')
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
